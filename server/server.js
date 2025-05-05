acff9b6292b883c251f4afaa7e7cd1ff1f51d0c6
Alvin Okoli <alvinokoli@gmail.com>
2025-05-05 22:50:42 +0100

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT;
 app.use(express.json());      
 app.use(express.static('public')); 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://country-rest-api-black.vercel.app/',
     methods: ['Get']
 }))
 
