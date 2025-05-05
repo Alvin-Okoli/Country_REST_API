5acf06a86d4a76441144b2540b264af4129a9228
Alvin <alvinokoli@gmail.com>
2025-05-05 22:42:23 +0100

    Updated Server api

--- Client/src/Components/loader/loader.jsx
+++ Client/src/Components/loader/loader.jsx
@@ -1,6 +1,6 @@
 export const loader = async ({params})=>{
         const {country} = params
-        const res = await fetch(`http://localhost:5000/country/${country}`);
+        const res = await fetch(`https://country-rest-api-tghk.onrender.com/country/${country}`);
         const data = await res.json()
         return {data}
     }
